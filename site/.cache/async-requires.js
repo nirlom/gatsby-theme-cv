5147bb21f5fdfdeda1030f6e3d32cd4c83f7e4db
Nirlom Team <tech@nirlom.com>
2019-09-15 18:38:41 +0530

    Delete extra files

--- site/.cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---node-modules-mhjadav-gatsby-theme-cv-src-templates-index-js": () => import("/Users/mj/Documents/Dreams/gatsby/gatsby-theme-building/site/node_modules/@mhjadav/gatsby-theme-cv/src/templates/index.js" /* webpackChunkName: "component---node-modules-mhjadav-gatsby-theme-cv-src-templates-index-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/mj/Documents/Dreams/gatsby/gatsby-theme-building/site/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
-}
-
